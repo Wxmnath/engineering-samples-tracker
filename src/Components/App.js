15d25b1e52b91a9b8ab51988a5e825b3cbd40824
Wxmnath <Wxmnath@aol.com>
2022-08-04 20:53:47 +0100

    Links added to App and log in and log out components created

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,31 @@
 import React from "react";
-import Nametitle from "./NameTitle";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import ProjectList from "./ProjectList";
+import Login from "./Login";
+import LogOut from "./LogOut";
 import "../Styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Engineering samples</header>
-      <Nametitle />
-      <ProjectList />
-    </div>
+    <Router>
+      <nav>
+        <Link className="Home" to="/">
+          Home
+        </Link>
+        <Link className="Login" to="login">
+          Login
+        </Link>
+        <Link className="Log-out" to="log_out">
+          Log Out
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ProjectList />} />
+        <Route path="login" element={<Login />} />
+        <Route path="log_out" element={<LogOut />} />
+        {/* <Route path=":id" element={<ProjectDisplay />} /> */}
+      </Routes>
+    </Router>
   );
 }
 

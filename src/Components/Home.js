504d8b3fe4c062a1b0ab770f3263f6b64a2ab66f
Wxmnath <Wxmnath@aol.com>
2022-12-01 18:12:47 +0000

    loading message added to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,7 @@ import ProjectList from "./ProjectList";
 
 function Home() {
   const [projects, setProjects] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8000/ProjectsData")
@@ -11,11 +12,13 @@ function Home() {
       })
       .then((data) => {
         setProjects(data);
+        setIsPending(false);
       });
   }, []); // square brackets to only render useEffect on the first render and not keep rendering after.
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {projects && (
         <ProjectList projects={projects} title="All Projects" /> // projectlist is firstly trying to map over null. by adding the logical and with projects being true will move over to call projectlist
       )}

9b7601602cc2525ea71437494c294fd6653ef7f5
Wxmnath <Wxmnath@aol.com>
2022-12-04 17:12:25 +0000

    react router added and create page added

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
-
 import Home from "./Home";
+import Create from "./Create";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 

b1b383476c5bcd8ce6650f78abe068e0a4b59563
Wxmnath <Wxmnath@aol.com>
2022-12-04 18:57:01 +0000

    fetch data added for individual projects created

--- src/Components/ProjectDetails.js
+++ src/Components/ProjectDetails.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 function ProjectDetails() {
   const { id } = useParams();
+  const {
+    data: project,
+    error,
+    isPending,
+  } = useFetch(`http://localhost:8000/ProjectsData/${id}`);
   return (
     <div className="project-details">
-      <h2>Project Details - {id}</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {project && (
+        <article>
+          <h2>{project.Part}</h2>
+          <div>Project Status: {project.Stage}</div>
+          <p>Lead engineer: {project.Engineer}</p>
+        </article>
+      )}
     </div>
   );
 }

f6c8cc8f8cffade7161e1bf036b8a065dbbaf8ff
Wxmnath <Wxmnath@aol.com>
2022-12-04 14:23:46 +0000

    error catch added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,20 +4,30 @@ import ProjectList from "./ProjectList";
 function Home() {
   const [projects, setProjects] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/ProjectsData")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("data not fetched??");
+        }
         return res.json();
       })
       .then((data) => {
         setProjects(data);
         setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
       });
   }, []); // square brackets to only render useEffect on the first render and not keep rendering after.
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {projects && (
         <ProjectList projects={projects} title="All Projects" /> // projectlist is firstly trying to map over null. by adding the logical and with projects being true will move over to call projectlist

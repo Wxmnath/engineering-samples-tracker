43ea5717bf3ceef9b2a45c471883b0b7c9aee704
Wxmnath <Wxmnath@aol.com>
2022-12-06 21:09:29 +0000

    delete button added to projectdetails and checklist component created. Issue trying to create checkbox handlechange

--- src/Components/ProjectDetails.js
+++ src/Components/ProjectDetails.js
@@ -1,5 +1,7 @@
+/* eslint-disable react/no-array-index-key */
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import CheckList from "./CheckList";
 import useFetch from "./useFetch";
 
 function ProjectDetails() {
@@ -9,15 +11,33 @@ function ProjectDetails() {
     error,
     isPending,
   } = useFetch(`http://localhost:8000/ProjectsData/${id}`);
+  const history = useNavigate();
+
+  const handleClickDelete = () => {
+    fetch(`http://localhost:8000/ProjectsData/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      history("/");
+    });
+  };
+
   return (
     <div className="project-details">
       {isPending && <div>Loading...</div>}
       {error && <div>{error}</div>}
       {project && (
         <article>
-          <h2>{project.Part}</h2>
+          <h1>{project.Customer}</h1>
+          <h2>
+            {project.Part} {project.Description}
+          </h2>
+          <h2>{project.Tool}</h2>
           <div>Project Status: {project.Stage}</div>
           <p>Lead engineer: {project.Engineer}</p>
+          <CheckList />
+          <button type="submit" onClick={handleClickDelete}>
+            Delete
+          </button>
         </article>
       )}
     </div>

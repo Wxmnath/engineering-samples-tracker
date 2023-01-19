4ae84f881d82d9cbc6a1d75ba3a7e4ece5a0cc17
Wxmnath <Wxmnath@aol.com>
2023-01-19 21:08:41 +0000

    added date to projectlist and sorted list with date

--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -3,6 +3,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function ProjectList({ projects, title }) {
+  projects.sort((a, b) => {
+    if (a.Date < b.Date) {
+      return -1;
+    }
+    if (a.Date > b.Date) {
+      return 1;
+    }
+    return 0;
+  });
+  //   if (a.Part === b.Part) {
+  //     return a.Date > b.Date;
+  //   }
+  //   return a.Customer > b.Customer ? 1 : -1;
+  // });
   // projects and title are props being called in Home component
   return (
     <div className="project-list">
@@ -11,8 +25,10 @@ function ProjectList({ projects, title }) {
         <div className="project-preview" key={project.id}>
           <Link to={`/projects/${project.id}`}>
             <h2>P/N {project.Part}</h2>
+            <p>{project.Customer}</p>
             <p>Tool {project.Tool}</p>
             <p>{project.Stage}</p>
+            <p>{project.Date}</p>
           </Link>
         </div>
       ))}
